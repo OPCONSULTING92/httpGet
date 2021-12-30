1311cec159043582289f447089f0a8e1283a7658
hamad penot <oussam73@hotmail.fr>
2021-12-30 12:08:41 +0100

    premierCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+ 
+export class Repos {
+  id!: string;
+  name!: string;
+  html_url!: string;
+  description!: string;
+}
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'httpGet';
+export class AppComponent implements OnInit {
+ 
+  // userName: string = "tektutorialshub"
+  userName: string = "OPCONSULTING92"
+  baseURL: string = "https://api.github.com/";
+  repos!: Repos[];
+ 
+  
+  constructor(private http: HttpClient) {
+  }
+ 
+  ngOnInit() {
+    this.getRepos()
+  }
+ 
+ 
+  public getRepos() {
+ 
+    return this.http.get<Repos[]>(this.baseURL + 'users/' + this.userName + '/repos')
+      .subscribe(
+        (response) => {                           //Next callback
+          console.log('response received')
+          console.log(response);
+          this.repos = response; 
+        },
+        (error) => {                              //Error callback
+          console.error('Request failed with error')
+          alert(error);
+        },
+        () => {                                   //Complete callback
+          console.log('Request completed')
+        })
+  }
 }
+ 
---
+++ src/app/repos.ts
@@ -0,0 +1,7 @@
+export class repos {
+    id: string | undefined;
+    name: string | undefined;
+    html_url: string | undefined;
+    description: string | undefined;
+}
+ 
