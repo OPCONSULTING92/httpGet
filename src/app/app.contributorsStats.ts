6651e1bef3ec74a8b52c7d904130f20764397bca
hamad penot <oussam73@hotmail.fr>
2022-01-04 10:33:24 +0100

    testMes

---
+++ src/app/app.contributorsStats.ts
@@ -0,0 +1,12 @@
+export class contributorStats {
+    total: number | undefined;
+    weeks: [weekActions] | undefined;
+}
+ 
+export class weekActions {
+    w:number| undefined;
+    a:number| undefined;
+    d:number| undefined;
+    c:number| undefined;
+
+}
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -5,6 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map, catchError} from 'rxjs/operators';
  
 import { repos } from './repos';
+
  
 @Injectable()
 export class GitHubService {
@@ -17,5 +18,24 @@ export class GitHubService {
   getRepos(userName: string): Observable<any> {
     return this.http.get(this.baseURL + 'users/' + userName + '/repos')
   }
+
+  getContributorStats(userName: string): Observable<any> {
+    return this.http.get(this.baseURL + 'users/' + userName + '/repos')
+  }
+  // 'use strict';
+
+// async function countGithub(repo) {
+//     const response = await fetch(`https://api.github.com/repos/${userName}/{repoName}/contributors`)
+//     const contributors = await response.json();
+//     const lineCounts = contributors.map(contributor => (
+//         contributor.weeks.reduce((lineCount, week) => lineCount + week.a - week.d, 0)
+//     ));
+//     const lines = lineCounts.reduce((lineTotal, lineCount) => lineTotal + lineCount);
+//     window.alert(lines);
+// }
+
+  // getLanguages(userName: string): Observable<any> {
+  //   return this.http.get(this.baseURL + 'users/' + userName + '/repos')
+  // }
  
 }
